fcd281b0414c74748e0f225cd21f217f84e4f0a7
dubbl <dubbel14@gmail.com>
2013-09-08 15:44:59 +0200

    Update config.php

--- c/php/config.php
+++ c/php/config.php
@@ -8,14 +8,14 @@
 
 #Configurationen
 #Datenbank:
-$benutzername="jh";
-$passwort="haschtag";
-$host="launchzone.de";
-$datenbank="jugendhackt_1";
+$benutzername="";
+$passwort="";
+$host="";
+$datenbank="";
 
 #Verbindung zur DB aufbauen
 $verbindung = mysql_connect($host, $benutzername , $passwort)
 or die("Verbindung zur Datenbank konnte nicht hergestellt werden");
 
-mysql_select_db($datenbank) or die ("Datenbank konnte nicht ausgewählt werden"); 
+mysql_select_db($datenbank) or die ("Datenbank konnte nicht ausgewï¿½hlt werden"); 
 ?>
